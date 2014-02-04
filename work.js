042e8e188c9a4b5fd1e6251dee963550d75dc870
Chyngyz Dyikanbaev <chipalino@live.com>
2014-02-04 16:30:20 +0600

    Create work.js

---
+++ work.js
@@ -0,0 +1,56 @@
+var abc = {
+	"MAC":"F4-6D-04-9C-D8-8C",
+	"proc":["2core","3 GHz"],
+	"RAM":"2 GB",
+	"HDD helth":"GOOD"
+}
+var biostar = {
+	"MAC":"00-30-67-D3-5D-44",
+	"proc":["2core","3 GHz"],
+	"RAM":"2 GB",
+	"HDD helth": "GOOD"
+}
+
+var wertyuiop = {
+	"MAC":"F8-D1-11-0A-9B-5A",
+	"proc":["2core","1.8 GHz"],
+	"RAM":"2 GB",
+	"HDD helth": "GOOD"
+}
+
+var buh1 = {
+	"MAC":"00-30-67-DE-D9-F6",
+	"proc":["2core","1.8 GHz"],
+	"RAM":"1 GB",
+	"HDD helth":"GOOD"
+}
+
+
+// one nore check
+var Yntymak = {
+	"MAC":"68-A3-C4-F6-00-C7",
+	"proc":["2core","1.8 GHz"],
+	"RAM":"2 GB",
+	"HDD helth":"GOOD"
+}
+var NURALY = {
+	"MAC":"C8-3A-35-CA-4C-71",
+	"proc":["2core","1.8 GHz"],
+	"RAM":"2 GB",
+	"HDD helth":"GOOD"
+}
+var dzxgx = {
+	"MAC":"00-30-67-DE-D2-1A",
+	"proc":["2core","1.8 GHz"],
+	"RAM":"2 GB",
+	"HDD helth":"GOOD"
+}
+var Ad123 = {
+	"MAC":"74-2F-68-42-A8-AD",
+	"proc":["2core","1.8 GHz"],
+	"RAM":"2 GB",
+	"HDD helth":"GOOD"
+}
+//wertyuiop - 
+//dzxgx - 
+//Ad123 - 
