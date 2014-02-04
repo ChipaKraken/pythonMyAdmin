9c7640a74af0ed70db252d5df4f0478c52218f7c
Chyngyz Dyikanbaev <chipalino@live.com>
2014-02-04 16:34:56 +0600

    Update work.js

--- work.js
+++ work.js
@@ -1,3 +1,4 @@
+//бухгалтерия
 var abc = {
 	"MAC":"F4-6D-04-9C-D8-8C",
 	"proc":["2core","3 GHz"],
@@ -11,12 +12,6 @@ var biostar = {
 	"HDD helth": "GOOD"
 }
 
-var wertyuiop = {
-	"MAC":"F8-D1-11-0A-9B-5A",
-	"proc":["2core","1.8 GHz"],
-	"RAM":"2 GB",
-	"HDD helth": "GOOD"
-}
 
 var buh1 = {
 	"MAC":"00-30-67-DE-D9-F6",
@@ -24,9 +19,15 @@ var buh1 = {
 	"RAM":"1 GB",
 	"HDD helth":"GOOD"
 }
+var dzxgx = {
+	"MAC":"00-30-67-DE-D2-1A",
+	"proc":["2core","1.8 GHz"],
+	"RAM":"2 GB",
+	"HDD helth":"GOOD"
+}
 
 
-// one nore check
+//Менеджерская
 var Yntymak = {
 	"MAC":"68-A3-C4-F6-00-C7",
 	"proc":["2core","1.8 GHz"],
@@ -39,11 +40,11 @@ var NURALY = {
 	"RAM":"2 GB",
 	"HDD helth":"GOOD"
 }
-var dzxgx = {
-	"MAC":"00-30-67-DE-D2-1A",
+var wertyuiop = {
+	"MAC":"F8-D1-11-0A-9B-5A",
 	"proc":["2core","1.8 GHz"],
 	"RAM":"2 GB",
-	"HDD helth":"GOOD"
+	"HDD helth": "GOOD"
 }
 var Ad123 = {
 	"MAC":"74-2F-68-42-A8-AD",
@@ -52,5 +53,5 @@ var Ad123 = {
 	"HDD helth":"GOOD"
 }
 //wertyuiop - 
-//dzxgx - 
+//dzxgx - back up server
 //Ad123 - 
